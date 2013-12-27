91023d5fe2ce2d27cad803917ed1e8aca0df255b
Vidhyanand <vidyanand.k@stellentsoft.com>
2013-12-27 15:28:06 +0530

    Initial Commit

---
+++ YoutubeSearch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YoutubeSearch
+//
+//  Created by Stellent Software on 12/27/13.
+//  Copyright (c) 2013 Stellent Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YoutubeSearch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  YoutubeSearch
+//
+//  Created by Stellent Software on 12/27/13.
+//  Copyright (c) 2013 Stellent Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
